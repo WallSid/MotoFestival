0d83b36fcf4606c9228ef0ccbe9f6f1b0af7b129
Vladyslav Savchuk <103652593+WallSid@users.noreply.github.com>
2023-01-18 01:34:50 +0200

    upd

--- js/popup.js
+++ js/popup.js
@@ -126,28 +126,111 @@ window.addEventListener("click", function(e) {
 
     if (e.target.dataset.action === "plus") {
       quantityValue.innerText = parseInt(++quantityValue.innerText);
+      calcPrice();
     };
     if ((e.target.dataset.action === "minus") && (parseInt(quantityValue.innerText) >= 1)) {
       quantityValue.innerText = parseInt(--quantityValue.innerText);
+      calcPrice();
     };
-    calcPrice();
+    
   }
 });
 
 function calcPrice() {
   const cardsWrapper = document.querySelectorAll("[data-popupWrapper]");
+  const totalPriceEl = document.querySelectorAll("[data-totalAmount]");
   
   cardsWrapper.forEach(function (item) {
     const cards = item.querySelectorAll("[data-card]");
+    let currentPrice;
+    let totalPrice = 0;
     cards.forEach(function (item) {
-      // const totalPrice = item.querySelector("[data-totalAmount]");
       const ticketPrice = item.querySelector("[data-price]");
-      console.log(ticketPrice);
-    });
+      const ticketQuantity = item.querySelector("[data-counter]");
 
-    // console.log(ticketPrice);
-    // totalPrice.innerText = 
+      currentPrice = parseInt(ticketPrice.innerText) * parseInt(ticketQuantity.innerText);
+      totalPrice = parseInt(currentPrice) + parseInt(totalPrice);
+      
+      
+      totalPriceEl.forEach(function (item) {
+        item.innerText = parseInt(totalPrice);
+      });
+    });
   });
-  console.log(cardsWrapper);
-  // console.log(totalPrice);
-}
+};
+
+// adding tickets to the next popup(cart)
+// adding tickets to the cart 
+const cartWrapper = document.querySelector('.submitBlock-popup__ticketsInner');
+
+// const cart = document.querySelector('._cartWrapper');
+// console.log(cart);
+window.addEventListener("click", function(event) {
+
+  if (event.target.hasAttribute('data-action')) {
+    
+    // finding card with ticket in which clicked
+    const card = event.target.closest('.card');
+    console.log(card);
+
+    //getting data from card and writting them in object productInfo
+    const productInfo = {
+      id: card.dataset.id,
+      price: card.querySelector('.cards-popup__price').textContent,
+      currentPrice: parseInt(card.querySelector('[data-price]').textContent) * parseInt(card.querySelector('[data-counter]').textContent),
+      date: card.querySelector('[data-title]').textContent,
+      day: card.querySelector('.cards-popup__subtitle').textContent,
+      counter: card.querySelector('[data-counter]').textContent,
+    };
+
+    
+    
+    // check if cart has the same ticket
+    const itemInCart = cartWrapper.querySelector(`[data-id="${productInfo.id}"]`);
+    const cards = document.querySelectorAll(`[data-id="${productInfo.id}"]`);
+    console.log(cards);
+    calcPrice();
+    console.log(itemInCart);
+    console.log(productInfo.currentPrice);
+    console.log(productInfo.price);
+    console.log(productInfo.id);
+    console.log(productInfo.date);
+    console.log(productInfo.counter);
+
+    if (itemInCart) {
+      const counterEl = itemInCart.querySelector('[data-counter]');
+      const priceEl = itemInCart.querySelector('[data-price]');
+
+     
+      if (event.target.dataset.action === "minus" && counterEl.textContent < 1) {
+            console.log('nemae!!!')
+            itemInCart.remove();
+            // cards.forEach(function(item) {
+            //   counterEl.innerText = 0;
+            // });
+          };
+          // if (event.target.dataset.action === "plus") {
+
+          // }
+
+    } else {
+        const cartItemHTML = `
+                              <div data-card data-id="${productInfo.id}" class="submitBlock-popup__tickets tickets card">
+                                <div class="ticket__date data-title date-ticket _ticketDate">
+                                    ${productInfo.date} <span class="date-ticket__accent _ticketDay">${productInfo.day}</span><span class="cards-popup__price" data-price>${productInfo.price}</span>
+                                </div>
+                                <div class="ticket__quantity quantity-tickets _amountWrapper">
+                                    <div data-cart data-action="minus" class="quantity-tickets__minus">-</div>
+                                    <div data-cart data-counter class="quantity-tickets__value">${productInfo.counter}</div>
+                                    <div data-cart data-action="plus" class="quantity-tickets__plus">+</div>
+                                </div>
+                                <div class="ticket__price">
+                                  ${productInfo.price}
+                                </div>
+                              </div>`;
+
+      // show in cart
+      cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+    }
+  }
+});
